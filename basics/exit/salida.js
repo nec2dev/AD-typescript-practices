c3185de325095249ddae26b45dfb1713b0737c4a
nec2dev <nec2multivackergithub@outlook.com>
2021-03-10 17:07:38 -0300

    my first commit

---
+++ basics/exit/salida.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+//types
+var myString = "hello";
+var myNumber = 22;
+var myBool = true;
+var myVar = "hello";
+myVar = 22;
+myVar = "22";
+myVar = true;
+//String
+document.write(myNumber.toString());
---
+++ basics/exit/salida.ts
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+//types
+
+var myString: string = "hello";
+var myNumber:number = 22;
+var myBool: boolean = true;
+var myVar: any = "hello";
+myVar = 22;
+myVar = "22";
+myVar = true;
+
+//String
+
+document.write(myNumber.toString());
